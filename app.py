82421762a3ab74ea9aaf37bdb767558fc5775369
Stanley <standrome@yahoo.com>
2022-09-24 23:34:49 -0700

    Adding file

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, url_for, redirect
+import csv
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+# def write_to_file(data):
+#     with open("database.txt", mode="a") as database:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         file = database.write(f"\n{email}, {subject}, {message}")
+
+def write_to_csv(data):
+    with open("database.csv", mode="a", newline="") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = csv.writer(database, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        file.writerow([email, subject, message])
+
+
+@app.route("/submit_form", methods=["POST", "GET"])
+def submit_form():
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+        except:
+            return "Error"
+    else:
+        return "Oh shit"
+
